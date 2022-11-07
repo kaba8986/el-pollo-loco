6ee05bbc4e7776fe654f0bb8f6e8351bbac738ea
Andreas Burghardt <andreas-burghardt@gmx.net>
2022-11-07 14:42:34 +0100

    start screen finished

--- js/script.js
+++ js/script.js
@@ -7,17 +7,18 @@ function startGame() {
     console.log('my character is ', world.character)
     document.getElementById('game-screen').classList.remove('d-none');
     document.getElementById('start-screen').classList.add('d-none');
+    document.getElementById('game-buttons').style.display = 'none';
 }
 
 
 function showControls() {
   document.getElementById('control-screen').style.display = 'flex';
-  document.querySelector('.ex-main').classList.add('d-none');
+  document.querySelector('.main-buttons').classList.add('d-none');
   document.getElementById('back').classList.remove('d-none');
 }
 
 function goBack() {
   document.getElementById('control-screen').style.display = 'none';
-  document.querySelector('.ex-main').classList.remove('d-none');
+  document.querySelector('.main-buttons').classList.remove('d-none');
   document.getElementById('back').classList.add('d-none');
 }
